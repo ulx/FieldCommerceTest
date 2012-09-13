9d79a55ca3b6dfb63c13ac200aa73d4f97d34289
alexander <alexander@alexander-Aspire-5930.(none)>
2012-09-13 20:02:38 +0400

    тесты для модели клиентов

--- src/ru/mbsl/fcdirektiva/test/TestModelClients.java
+++ src/ru/mbsl/fcdirektiva/test/TestModelClients.java
@@ -10,14 +10,20 @@ import android.test.AndroidTestCase;
 import android.test.suitebuilder.annotation.MediumTest;
 
 public class TestModelClients extends AndroidTestCase  {
-	ClientsModel model = FactoryModel.getClientsModel();
-	
+	ClientsModel model = FactoryModel.getClientsModel();	
+	/**
+	 * Выполняем операции перед прогоном тестов
+	 */
 	@Override
 	protected void setUp() throws Exception {
+		// Инициализируем менаджер с контекстом тестов
 		DataBaseManager.getInstance(getContext());
 		super.setUp();
 	}
 	
+	/**
+	 * Сохраннение элемента в таблицу
+	 */
 	@MediumTest
 	public void testSaveInsert(){			
 		ClientItem item = new ClientItem();
@@ -25,13 +31,77 @@ public class TestModelClients extends AndroidTestCase  {
 		assertTrue(index > 0);
 	}
 
+	/**
+	 * Очистка всей таблицы
+	 */
+	@MediumTest
 	public void testClearTable(){
 		ClientItem item = new ClientItem();
 		model.save(item);		
 		model.clearDataBase();
-		List<ClientItem> list = model.getItemsAll();
+		List<?> list = model.getItemsAll();
 		assertTrue(list.size() == 0);
 	}
 
+	/**
+	 * Удаление элемента
+	 */
+	@MediumTest
+	public void testDeleteItem(){
+		ClientItem item = new ClientItem();		
+		item._id = model.save(item);
+		int index = model.removeItem(item._id);
+		assertEquals(1, index);
+	}
+	
+	/**
+	 * Получение списка элементов
+	 */
+	@MediumTest
+	public void testGetItemsAll(){
+		model.clearDataBase();
+		ClientItem item = new ClientItem();
+		model.save(item);
+		model.save(item);
+		model.save(item);
+		model.save(item);
+		List<?> list = model.getItemsAll();
+		assertTrue(list.size() == 4);
+	}
+	
+	/**
+	 * Получение элемента по идентификатору
+	 */
+	@MediumTest
+	public void testGetItem(){
+		ClientItem item = new ClientItem();
+		item.address = "test";
+		item.group_id = 1;
+		item.name = "test";
+		item.status = 1;
+		item._id = model.save(item);
+		ClientItem temp = model.getItem(item._id);
+		assertTrue(item.equals(temp));
+	}
+	
+	/**
+	 * Проверка обновления элемента в базе
+	 */
+	@MediumTest
+	public void testUpdateItem(){
+		ClientItem item = new ClientItem();
+		item.address = "test";
+		item.group_id = 1;
+		item.name = "test";
+		item.status = 1;
+		item._id = model.save(item);
+		item.address = "test2";
+		item.group_id = 12;
+		item.name = "test2";
+		item.status = 12;
+		model.save(item);
+		ClientItem temp = model.getItem(item._id);
+		assertTrue(item.equals(temp));
+	}
 	
 }
